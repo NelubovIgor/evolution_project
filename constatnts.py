7c2e8869e2acc3bde40bc98e9a7ff425206e7e65
Igor <nelubov.igor@gmail.com>
2025-02-23 10:25:27 +0300

    change struct project

---
+++ constatnts.py
@@ -0,0 +1,14 @@
+WIDTH = 800
+HEIGHT = 600
+SIDEBAR_WIDTH = 200
+
+FPS = 30
+
+CELL_SIZE = 1
+
+CYAN = (0, 128, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
---
+++ objects.py
@@ -0,0 +1,63 @@
+import random, pygame
+from constatnts import *
+
+
+class Body:
+    all_bodies = {}
+
+    def __init__(self, x, y, color, birthday, energy=100, size=CELL_SIZE):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.birthday = birthday
+        self.energy = energy
+        self.size = size
+        Body.all_bodies[(x, y)] = self
+
+    def update_coordinates(self, new_crd):
+        Body.clear_body(self)
+        Body.all_bodies[new_crd] = self
+
+    def clear_body(self):
+        del Body.all_bodies[(self.x, self.y)]
+
+    def random_coordinates():
+        while True:
+            x = random.randint(0, WIDTH)
+            y = random.randint(0, HEIGHT)
+            if (x, y) not in Body.all_bodies:
+                return x, y
+            
+    def collision(self):
+        pass
+
+class Player(Body):
+    def __init__(self, x, y, birthday, color=BLUE):
+        super().__init__(x, y, color, birthday)
+
+    def move_player(self, pressed):
+        speed = 1
+        if pressed[pygame.K_w]:
+            Body.update_coordinates(self, (self.x, self.y - speed))
+            self.y -= speed
+        if pressed[pygame.K_s]:
+            Body.update_coordinates(self, (self.x, self.y + speed))
+            self.y += speed
+        if pressed[pygame.K_a]:
+            Body.update_coordinates(self, (self.x - speed, self.y))
+            self.x -= speed
+        if pressed[pygame.K_d]:
+            Body.update_coordinates(self, (self.x + speed, self.y))
+            self.x += speed
+
+class Grass(Body):
+    def __init__(self, x, y, birthday, color=GREEN, size=CELL_SIZE):
+        super().__init__(x, y, color, birthday, size)
+
+class Predator(Body):
+    def __init__(self, x, y, birthday, color=RED):
+        super().__init__(x, y, color, birthday)
+
+class Herbivore(Body):
+    def __init__(self, x, y, birthday, color=CYAN):
+        super().__init__(x, y, color, birthday)
