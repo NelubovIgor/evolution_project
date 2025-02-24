b17a0e261d7284d274c7187fac43344c789311d2
Igor <nelubov.igor@gmail.com>
2025-02-24 13:23:08 +0300

    wrong way

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-n = "А1_23ОВ_45"
-
-lit = 'АВЕКМНОРСТУХ'
-num = n.split('_')
-if len(num) == 2:
-    if len(num[0]) == 6 and (2 <= len(num[1]) <= 3):
-        if num[0][0] in lit and num[0][1:4].isdigit() and num[0][4] in lit and num[0][5] in lit and num[1].isdigit():
-            print("YES")
-        else:
-            print("NO")
-    else:
-        print("NO")
-else:
-    print("NO")
+print(chr(max([[sum([ord(s) for s in symb]) for symb in input() for _ in range(4)]])))
