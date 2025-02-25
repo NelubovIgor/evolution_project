14b9334edd6e654b897ce5cb08fa6656b432759a
Igor <nelubov.igor@gmail.com>
2025-02-25 13:23:02 +0300

    wrong way1

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print(chr(max([[sum([ord(s) for s in symb]) for symb in input() for _ in range(4)]])))
+import re
+
+text = "Hello, my name is [u-1061][u-1072][u-1082][u-1080]!"
+
+while True:
+    indx = text.find('[u-')
+    if indx == -1: break
+    print(text[0:indx])
+    print(chr(int(text[indx+3:indx+7])))
+    print(text[indx+8:])
+    text = text[0:indx] + chr(int(text[indx+3:indx+7])) + text[indx+8:]
+
+print(text)
