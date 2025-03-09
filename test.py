50d97f33383edd6694eae58de822b71be0a44f1b
Igor <nelubov.igor@gmail.com>
2025-03-09 14:07:00 +0300

    holiday1!

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import random
 from constants import *
 
-print(tuple(a + b for a, b in zip(random.choice(list(DIRECTIONS.values())), (30, 34))))
+
+# print(tuple(a + b for a, b in zip(random.choice(list(DIRECTIONS.values())), (30, 34))))
