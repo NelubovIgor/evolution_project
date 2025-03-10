ed5358737782a1ed69d0c8dfb4fef28ba51c7816
Igor <nelubov.igor@gmail.com>
2025-03-10 12:53:27 +0300

    tests1

--- test.py
+++ test.py
@@ -1,5 +1,34 @@
 import random
 from constants import *
+import datetime
 
+class WeatherWarning:
+    def rain(self):
+        print("Ожидаются сильные дожди и ливни с грозой")
+    def snow(self):
+        print("Ожидается снег и усиление ветра")
+    def low_temperature(self):
+        print("Ожидается сильное понижение температуры")
+
+class WeatherWarningWithDate(WeatherWarning):
+    def rain(self, date):
+        print(date)
+        super().rain()
+    def snow(self, date):
+        super().snow()
+        print(date)
+    def low_temperature(self, date):
+        super().low_temperature()
+        print(date)
+
+
+from datetime import date
+
+weatherwarning = WeatherWarningWithDate()
+dt = date(2022, 12, 12)
+
+weatherwarning.rain(dt)
+weatherwarning.snow(dt)
+weatherwarning.low_temperature(dt)
 
 # print(tuple(a + b for a, b in zip(random.choice(list(DIRECTIONS.values())), (30, 34))))
