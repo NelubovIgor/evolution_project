ce87590c94b9f66810acb53c906cdb1760fe6ebe
Igor <nelubov.igor@gmail.com>
2025-03-21 06:30:11 +0300

    before rework10

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 import random
 from constants import *
 
+class LowerCaseDict(dict):
+    def __setitem__(self, key, value):
+        key = str(key).lower()
+        super().__setitem__(key, value)
+
+
+lowercasedict = LowerCaseDict({'ONE': 1})
+lowercasedict.update({'TWO': 2})
+
+print(lowercasedict)
