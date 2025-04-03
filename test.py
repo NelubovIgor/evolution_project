ac2d4d1ba2c788ceedf89677c37fd97c2defe663
Igor <nelubov.igor@gmail.com>
2025-04-03 10:06:49 +0300

    zero energy

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import random
 from constants import *
 
-n = 0
-while True:
-    n += 1
-    if 3 * n + 100 == 6 * n + 10:
-        break
-print(n) 
+def draw_graph(f):
+    for x in range(13):
+        print()
+        for y in range(12):
+            print(x, end="")
+
+draw_graph(lambda x: x)
