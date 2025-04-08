8230012e136d40f74a08b9678e8b89b1b4060100
Igor <nelubov.igor@gmail.com>
2025-04-08 11:08:38 +0300

    zero energy

--- test.py
+++ test.py
@@ -7,5 +7,8 @@ def draw_graph(f):
         for y in range(12):
             print(x, end="")
 
-draw_graph(lambda x: x * 1)
+# draw_graph(lambda x: x * 1)
 
+print(-16 / -5)
+x = 7
+print(4 * (x - 1) + 3 * x - 3 == 7 * (x - 1))
