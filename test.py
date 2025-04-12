9321eecae71859dcfc95866d162f409c7c7f50f7
Igor <nelubov.igor@gmail.com>
2025-04-12 20:25:41 +0300

    tasks

--- test.py
+++ test.py
@@ -1,31 +1,34 @@
 import random, math, itertools
 from constants import *
 
-def quadratic_intersections(a1, b1, c1, a2, b2, c2):
-    points = set()
-    a = a1 - a2
-    b = b1 - b2
-    c = c1 - c2
-    d = b ** 2 - 4 * a * c
+def intersections(a, b, c, k, m):
+    points = list()
+    bk = b - k
+    cm = c - m
+    d = bk ** 2 - 4 * a * cm
     x1 = None
     x2 = None
     if d >= 0 and a != 0:
-        x1 = (-b - math.sqrt(d)) / (2 * a)
-        if d > 0:
-            x2 = (-b + math.sqrt(d)) / (2 * a)
-    if x1 != None:
-        y1 = a1 * x1 ** 2 + b1 * x1 + c1
-        points.add((x1, y1))
-        if x2 != None:
-            y2 = a2 * x2 ** 2 + b2 * x2 + c2
-            points.add((x2, y2))
-    if a == 0 and b != 0:
-        x = -(c / b)
-        y = a * x ** 2 + b * x + c
-        points.add((x, y))
-    return points
+        if d == 0:
+            x3 = -bk / (2 * a)
+            y3 = a * x3 ** 2 + bk * x3 + cm
+            points.append((x3, y3))
+        elif d > 0:
+            x1 = (-bk + math.sqrt(d)) / (2 * a)
+            y1 = a * x1 ** 2 + bk * x1 + cm
+            points.append((x1, y1))
+            x2 = (-bk - math.sqrt(d)) / (2 * a)
+            y2 = a * x2 ** 2 + bk * x2 + cm
+            points.append((x2, y2))
+    if a == 0:
+        x = -cm / bk
+        y = a * x ** 2 + bk * x + cm
+        points.append((x, y))
+    return set(points)
 
-# print(quadratic_intersections(1, 2, 3, 1, 1, 4))
+# print(intersections(1, 0, 0, 1, 0)) #{(0.0, 0.0), (1.0, 1.0)}
+# print(intersections(1, -3, 2, 1, -1)) #{(1.0, 0.0), (3.0, 2.0)}
+
 
 def polynomial_sum(p1, p2):
     pp = {}
@@ -47,28 +50,36 @@ def polynomial_sum(p1, p2):
 
     return tuple(reversed(result))
 
-p1 = (2, -4, 5)                  # P1(x) = 2x^2 - 4x + 5
-p2 = (3, 2)                      # P2(x) = 3x + 2
+# p1 = (2, -4, 5)                  # P1(x) = 2x^2 - 4x + 5
+# p2 = (3, 2)                      # P2(x) = 3x + 2
 
-print(polynomial_sum(p1, p2))    # P1(x) + P2(x) = 2x^2 - x + 7
+# print(polynomial_sum(p1, p2))    # P1(x) + P2(x) = 2x^2 - x + 7
 
-p1 = (1, 7, 0, -4)               # P1(x) = x^3 + 7x^2 - 4
-p2 = (-1, 0, 0, 2)               # P2(x) = -x^3 + 2
+# p1 = (1, 7, 0, -4)               # P1(x) = x^3 + 7x^2 - 4
+# p2 = (-1, 0, 0, 2)               # P2(x) = -x^3 + 2
 
-print(polynomial_sum(p1, p2))    # P1(x) + P2(x) = 7x^2 - 2
+# print(polynomial_sum(p1, p2))    # P1(x) + P2(x) = 7x^2 - 2
 
-p1 = (1, 2, 3, 4, 5)             # P1(x) = x^4 + 2x^3 + 3x^2 + 4x + 5
-p2 = (1,)                        # P2(x) = 1
+# p1 = (1, 2, 3, 4, 5)             # P1(x) = x^4 + 2x^3 + 3x^2 + 4x + 5
+# p2 = (1,)                        # P2(x) = 1
 
-print(polynomial_sum(p1, p2))    # P1(x) + P2(x) = x^4 + 2x^3 + 3x^2 + 4x + 6
+# print(polynomial_sum(p1, p2))    # P1(x) + P2(x) = x^4 + 2x^3 + 3x^2 + 4x + 6
 
-p1 = (1, 1, 1, 1)                # P1(x) = x^3 + x^2 + x + 1
-p2 = (-1, -1, -1, -1, -1)        # P2(x) = -x^4 - x^3 - x^2 - x - 1
+# p1 = (1, 1, 1, 1)                # P1(x) = x^3 + x^2 + x + 1
+# p2 = (-1, -1, -1, -1, -1)        # P2(x) = -x^4 - x^3 - x^2 - x - 1
 
-print(polynomial_sum(p1, p2))    # P1(x) + P2(x) = -x^4
+# print(polynomial_sum(p1, p2))    # P1(x) + P2(x) = -x^4
 
-nums = [(1-(1/n**2)) for n in range(2, 11)]
-result = 1
-for n in nums:
-    result *= n
-print(result)    
+def time_zone(h, a, b):
+    difference = 0
+    if a > b: difference = a - b
+    elif a < b: difference = b - a
+    if difference == h: return 0
+    else: difference += h
+    if 23 > difference: return difference
+    else: return difference % 23
+
+print(time_zone(12, 3, 7))
+print(time_zone(6, -11, 12))
+print(time_zone(23, 12, -11))
+print(time_zone(11, 12, -11))
