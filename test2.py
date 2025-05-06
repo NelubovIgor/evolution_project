22abcbcae323cb418bacce47dcf56e3d90207291
Igor <nelubov.igor@gmail.com>
2025-05-06 09:54:30 +0300

    tests

--- test2.py
+++ test2.py
@@ -4,13 +4,4 @@ def snake(n):
 # snake(3)
 
 
-def mystery(n):
-    donats = 0
-    for s in str(n):
-        if s == "6" or s == "9" or s == "0": donats += 1
-        elif s == "8": donats += 2    
-    return donats
 
-# print(mystery(8))
-
-print(((1 - 3 ** 8) * 7) / 2)
