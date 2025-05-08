3937b602ec4fa261a2f3528083c631c5b27efe53
Igor <nelubov.igor@gmail.com>
2025-05-08 11:56:56 +0300

    tests

--- test2.py
+++ test2.py
@@ -1,7 +1,11 @@
-from itertools import zip_longest
-
 def polynomial(p):
     result = ''
-    return 
+    for i, s in enumerate(reversed(p)):
+        if i == 0 and s != 0:
+            if s > 0:
+                result += str(s) + "+"
+            else:
+                result += str(abs(s)) + "-"
+    return result
 
 print(polynomial((1, 3, -1, 1, -2)))
